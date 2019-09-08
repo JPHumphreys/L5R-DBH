2de316487ec44e2623be25e465faa427169b8810
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2019-09-08 19:43:06 +0100

    first commit - basic front end

---
+++ JS/decks.js
@@ -0,0 +1,8 @@
+//COLORS
+const rgb_crab = [124,135,146];
+const rgb_crane = [101,171,213];
+const rgb_dragon = [96,158,124];
+const rgb_lion = [191,160,48];
+const rgb_phoenix = [218,145,84];
+const rgb_scorpion = [158,62,43];
+const rgb_unicorn = [138,101,142];
---
+++ JS/modules/deckbuilder_variables.js
@@ -0,0 +1,5 @@
+export const fire = document.getElementById('fire-button');
+export const water = document.getElementById('water-button');
+export const earth = document.getElementById('earth-button');
+export const air = document.getElementById('air-button');
+export const voi = document.getElementById('void-button');
