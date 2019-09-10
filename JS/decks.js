c42567fda12b175bb119a1f5eacb14f15b724283
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2019-09-10 18:12:55 +0100

    second wave of code quality changes

--- JS/decks.js
+++ JS/decks.js
@@ -1,8 +1,8 @@
 //COLORS
-const rgb_crab = [124,135,146];
-const rgb_crane = [101,171,213];
-const rgb_dragon = [96,158,124];
-const rgb_lion = [191,160,48];
-const rgb_phoenix = [218,145,84];
-const rgb_scorpion = [158,62,43];
-const rgb_unicorn = [138,101,142];
+const rgbCrab = [124,135,146];
+const rgbCrane = [101,171,213];
+const rgbDragon = [96,158,124];
+const rgbLion = [191,160,48];
+const rgbPhoenix = [218,145,84];
+const rgbScorpion = [158,62,43];
+const rgbUnicorn = [138,101,142];
