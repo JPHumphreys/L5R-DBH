9f8a9515f9d33af364e06f2b777455184a94ca93
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2019-09-15 18:14:57 +0100

    fixed vs functions

--- JS/vs-clan.js
+++ JS/vs-clan.js
@@ -20,100 +20,48 @@ var vs_clans = {
 
 
 function versusClan(ele){
-    let element = ele.innerText;
+   
+    let element = document.getElementById(ele.id);
 
-    switch(element){
+    if(element.classList.contains("btn-light")){
+        element.classList.remove("btn-light");
+        element.classList.add("btn-danger");
+    }
+    else{
+        element.classList.remove("btn-danger");
+        element.classList.add("btn-light");
+    }
+    updateVSClans(element);
+}
+
+function updateVSClans(obj){
+
+    let clan = obj.innerText;
+
+    switch(clan){
         case "Crab":
-            if(vs_clans.crab !== true){
-                vsCrab.classList.remove("btn-light");
-                vsCrab.classList.add("btn-danger");
-                vs_clans.crab = true;
-            }
-            else{
-                vsCrab.classList.remove("btn-danger");
-                vsCrab.classList.add("btn-light");
-                vs_clans.crab = false;
-            }
-            //console.log(vs_clans);
+            vs_clans.crab = (vs_clans.crab === true) ? false : true;
         break;
         case "Crane":
-            if(vs_clans.crane !== true){
-                vsCrane.classList.remove("btn-light");
-                vsCrane.classList.add("btn-danger");
-                vs_clans.crane = true;
-            }
-            else{
-                vsCrane.classList.remove("btn-danger");
-                vsCrane.classList.add("btn-light");
-                vs_clans.crane = false;
-            }
-            //console.log(vs_clans);
+            vs_clans.crane = (vs_clans.crane === true) ? false : true;
         break;
         case "Dragon":
-            if(vs_clans.dragon !== true){
-                vsDragon.classList.remove("btn-light");
-                vsDragon.classList.add("btn-danger");
-                vs_clans.dragon = true;
-            }
-            else{
-                vsDragon.classList.remove("btn-danger");
-                vsDragon.classList.add("btn-light");
-                vs_clans.dragon = false;
-            }
-            //console.log(vs_clans);
+            vs_clans.dragon = (vs_clans.dragon === true) ? false : true;
         break;
         case "Lion":
-            if(vs_clans.lion !== true){
-                vsLion.classList.remove("btn-light");
-                vsLion.classList.add("btn-danger");
-                vs_clans.lion = true;
-            }
-            else{
-                vsLion.classList.remove("btn-danger");
-                vsLion.classList.add("btn-light");
-                vs_clans.lion = false;
-            }
-            //console.log(vs_clans);
+            vs_clans.lion = (vs_clans.lion === true) ? false : true;
         break;
         case "Phoenix":
-            if(vs_clans.phoenix !== true){
-                vsPhoenix.classList.remove("btn-light");
-                vsPhoenix.classList.add("btn-danger");
-                vs_clans.phoenix = true;
-            }
-            else{
-                vsPhoenix.classList.remove("btn-danger");
-                vsPhoenix.classList.add("btn-light");
-                vs_clans.phoenix = false;
-            }
-            //console.log(vs_clans);
+            vs_clans.phoenix = (vs_clans.phoenix === true) ? false : true;
         break;
         case "Scorpion":
-            if(vs_clans.scorpion !== true){
-                vsScorpion.classList.remove("btn-light");
-                vsScorpion.classList.add("btn-danger");
-                vs_clans.scorpion = true;
-            }
-            else{
-                vsScorpion.classList.remove("btn-danger");
-                vsScorpion.classList.add("btn-light");
-                vs_clans.scorpion = false;
-            }
-            //console.log(vs_clans);
+            vs_clans.scorpion = (vs_clans.scorpion === true) ? false : true;
         break;
         case "Unicorn":
-            if(vs_clans.unicorn !== true){
-                vsUnicorn.classList.remove("btn-light");
-                vsUnicorn.classList.add("btn-danger");
-                vs_clans.unicorn = true;
-            }
-            else{
-                vsUnicorn.classList.remove("btn-danger");
-                vsUnicorn.classList.add("btn-light");
-                vs_clans.unicorn = false;
-            }
-            //console.log(vs_clans);
+            vs_clans.unicorn = (vs_clans.unicorn === true) ? false : true;
         break;
-        
+        default:
+            alert("something went wrong with selecting vs clan");
+        break;
     }
 }
