0af859a93c5713c9c989af1ad755dcdf448d1c07
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2019-09-15 15:42:40 +0100

    fixed the element function

--- JS/keeper-seeker.js
+++ JS/keeper-seeker.js
@@ -28,74 +28,16 @@ function roleCheck(ele){
     }
 }
 
-function roleSelect(ele){
-    if(ele.innerText == "Keeper"){
-        //set new button type
-        keeper.classList.remove("btn-light");
-        keeper.classList.add("btn-danger");
-        //put old as old
-        seeker.classList.remove("btn-danger");
-        seeker.classList.add("btn-light");
-    }
-    else{
-        //set new button type
-        seeker.classList.remove("btn-light");
-        seeker.classList.add("btn-danger");
-        //put old and old
-        keeper.classList.remove("btn-danger");
-        keeper.classList.add("btn-light");
-    }
-    
+
+function elementCheck(ele){
+
+    let element = document.getElementById(ele.id);
+
+    element.classList.remove("btn-light");
+    element.classList.add("btn-danger");
+    //set others back
+    lastElement.classList.remove("btn-danger");
+    lastElement.classList.add("btn-light");
+    lastElement = element;
 }
 
-function elementSelect(ele){
-    element = ele.innerText;
-
-     switch(element){
-         case "Fire":
-            //set this to danger
-            fire.classList.remove("btn-light");
-            fire.classList.add("btn-danger");
-            //set others back to light
-            lastElement.classList.remove("btn-danger");
-            lastElement.classList.add("btn-light");
-            lastElement = fire;
-         break;
-         case "Air":
-            //set this to danger
-             air.classList.remove("btn-light");
-             air.classList.add("btn-danger");
-            //set others back to light
-             lastElement.classList.remove("btn-danger");
-             lastElement.classList.add("btn.light");
-             lastElement = air;
-         break;
-         case "Earth":
-         //set this to danger
-             earth.classList.remove("btn-light");
-             earth.classList.add("btn-danger");
-         //set others back to light
-             lastElement.classList.remove("btn-danger");
-             lastElement.classList.add("btn.light");
-             lastElement = earth;
-         break;
-         case "Water":
-         //set this to danger
-             water.classList.remove("btn-light");
-             water.classList.add("btn-danger");
-         //set others back to light
-             lastElement.classList.remove("btn-danger");
-             lastElement.classList.add("btn.light");
-             lastElement = water;
-         break;
-         case "Void":
-         //set this to danger
-             voi.classList.remove("btn-light");
-             voi.classList.add("btn-danger");
-         //set others back to light
-             lastElement.classList.remove("btn-danger");
-             lastElement.classList.add("btn.light");
-             lastElement = voi;
-        break;
-     }
-}
