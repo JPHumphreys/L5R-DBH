803dc0143b74455500a6e57a9f8c3424b503dca8
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2019-12-17 17:09:12 +0000

    made the hover effect better through only using 1 class now

--- JS/cards.js
+++ JS/cards.js
@@ -1,11 +1,22 @@
 const cardRenderLocation = document.getElementById("card-render-location");
 
-function hoverBlurr(element){
-    let parent = element.parentElement;
+function hoverBlurr(element, type){
     
-    for(let i = 0; i < parent.children.length; i++){
-        parent.children[i].classList.toggle("hover-row");
-    }
+    let parent = element.parentElement;
+    switch(type){
+            case "enter": 
+            for(let i = 0; i < parent.children.length; i++){
+                parent.children[i].classList.add("hover-row");
+            }
+            return;
+            case "leave":
+                for(let i = 0; i < parent.children.length; i++){
+                parent.children[i].classList.remove("hover-row");
+            }
+            return;
+            default: console.alert("something went wrong");
+                return;
+        } 
 }
 
 function cardClick(element){
