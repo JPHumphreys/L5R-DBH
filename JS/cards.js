4c5d41325ef2135169eedd28f1c7883dc4c66865
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2019-12-23 17:41:36 +0000

    created a function to remove an element that matches whats passed in.

--- JS/cards.js
+++ JS/cards.js
@@ -32,16 +32,24 @@ function Card(){
 
 let card = new Card();
 
-function addElement(deckProperty, newElement){
+function addElement(property, newElement){
     let counter = 0 ;
-    for(let value in deckProperty){
-        if(newElement != deckProperty[value]){
+    for(let value in property){
+        if(newElement != property[value]){
             counter++;
         }
     }
     //* if counter does not equal length - then 1 element matched *//
-    if(counter === deckProperty.length){
-        deckProperty.push(newElement);
+    if(counter === property.length){
+        property.push(newElement);
+    }
+}
+
+function removeElement(property, element){
+    for(let value in property){
+        if(element === property[value]){
+            property.splice(value, 1);
+        }
     }
 }
 
