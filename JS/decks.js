f12b7e65bc9efe09a766d0bf2f6feab6fb845caf
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-01-04 16:56:55 +0000

    removed the old build deck function and comments

--- JS/decks.js
+++ JS/decks.js
@@ -90,19 +90,6 @@ function createPrimaryAndRole(primary,role){
     deckRole.appendChild(roleImage);
     primaryAndRole.appendChild(deckRole)
 
-    /*
-
-            <div class="row primary-role">
-                <div class="col-8 deck-primary" data-tooltip="The Primary Clan of this deck is Crab.">
-                  <h4>Crab</h4>
-                </div>
-                <div class="col-3 deck-role" data-tooltip="Role is Keeper">
-                  <img src="/media/keeper_colour.jpg" alt="">
-                </div>
-            </div>
-
-    */
-
     return primaryAndRole;
 }
 
@@ -130,18 +117,6 @@ function createSecondaryandElement(secondary, element){
 
     return secondaryAndElement;
 
-    /*
-            <div class="row secondary-element">
-                  <div class="col-8 deck-secondary"  data-tooltip="The Secondary Clan is Phoenix.">
-                    <h5>Phoenix</h5>
-                  </div>
-                  <div class="col-3 deck-element" data-tooltip="Element is Fire">
-                    <img src="/media/fire_colour.jpg" alt="">
-                  </div>
-                  
-              </div>
-
-    */
 }
 
 function createDeckButtons(){
@@ -163,13 +138,6 @@ function createDeckButtons(){
     buttonRow.appendChild(copy);
     buttonRow.appendChild(remove);
     return buttonRow;
-    /*
-            <div class="row btn-group button-row">
-                <button class="btn btn-md btn-outline-dark rename-button">Rename</button>
-                <button class="btn btn-md btn-outline-dark copy-button">Copy</button>
-                <button class="btn btn-md btn-outline-dark delete-button">Delete</button>
-            </div>
-    */
 
 }
 
@@ -188,120 +156,6 @@ function buildDeck(deckname, primary, secondary, role, element){
     deck.appendChild(createSecondaryandElement(secondary, element));
     deck.appendChild(createDeckButtons());
     deckSpawnLocation.append(deck);
-
-    /*
-
-        <div class="col-sm-12 col-md-6 col-lg-3 deck">
-
-              <h3 class="deckname">Deckname</h3>
-  
-              
-  
-        </div>
-
-
-    */
-
-}
-
-function deckBuilder(deckname,priclan, splashclan, textObj){
-
-    let randomId = randomString(16);//16 random characters - unlikeliness of same deckname
-
-    let deck = document.createElement("div");
-    deck.classList.add("col-sm-12");
-    deck.classList.add("col-md-6");
-    deck.classList.add("col-lg-4");
-    deck.classList.add(randomId);
-    deck.classList.add("filter");
-
-    let deckChild = document.createElement("div");
-    deckChild.classList.add("card");
-    deckChild.classList.add("mb-4");
-    deckChild.classList.add("deck");
-
-    let deckBody = document.createElement("div");
-    deckBody.classList.add("card-body");
-    deckBody.classList.add("text-center");
-
-    let deckName = document.createElement("h3");
-    deckName.innerText = deckname;
-    deckName.id = randomId + "-update";
-
-    let priElement = document.createElement("h5");
-    priElement.innerText = "Primary Clan : " + priclan;
-
-    let splashElement = document.createElement("p");
-    splashElement.innerText = "Splash Clan : " + splashclan;
-
-    let buttonRow = document.createElement("div");
-    buttonRow.classList.add("row");
-
-    let updateButton = document.createElement("button");
-    updateButton.type = "button";
-    updateButton.classList.add("btn");
-    updateButton.classList.add("btn-primary");
-    updateButton.classList.add("deck-buttons");
-    updateButton.innerText = "Rename";
-    idToUpdate.push(randomId + "-update");
-    updateButton.addEventListener("click",function(){
-        callDecknameUpdateModal();
-    });
-
-    let copyButton = document.createElement("button");
-    copyButton.type = "button";
-    copyButton.classList.add("btn");
-    copyButton.classList.add("btn-primary");
-    copyButton.classList.add("deck-buttons");
-    copyButton.innerText = "Copy";
-    copyButton.addEventListener("click", function(){
-        handleCopyDeck();
-    })
-
-    let deleteButton = document.createElement("button");
-    deleteButton.type = "button";
-    deleteButton.classList.add("btn");
-    deleteButton.classList.add("btn-danger");
-    deleteButton.classList.add("deck-buttons");
-    deleteButton.innerText = "Delete";
-    deleteButton.id = randomId;
-    idToDelete.push(randomId);
-    deleteButton.addEventListener("click", function(){
-        handleDeleteDeck(this);
-    });
-
-    let textChunk = document.createElement("div");
-    textChunk.id = "textChunk";
-    textChunk.hidden = true;
-    let space = document.createElement("br");
-    textChunk.appendChild(space);
-
-    for(let i = 0; i < textObj.length; i++){
-        let text = document.createElement("p");
-        text.innerText = textObj[i];
-        textChunk.append(text);
-    }
-
-    // create the deck
-
-    buttonRow.appendChild(updateButton);
-    buttonRow.appendChild(copyButton);
-    buttonRow.appendChild(deleteButton);
-
-    deckBody.appendChild(deckName);
-    deckBody.appendChild(priElement);
-    deckBody.appendChild(splashElement);
-    deckBody.appendChild(buttonRow);
-    deckBody.appendChild(textChunk);
-
-    deckChild.appendChild(deckBody);
-
-    deck.appendChild(deckChild);
-
-    deckSpawnLocation.append(deck);
-
-    deckData.deckName = deckname;
-    deckData.deckCopy = "";
 
 }
 
