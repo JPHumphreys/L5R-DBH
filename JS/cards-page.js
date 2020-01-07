56acc77d4b22eaefe4310afc07efd487d1789535
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-01-07 10:17:46 +0000

    moved code that should belong in the cards api from the cards page

--- JS/cards-page.js
+++ JS/cards-page.js
@@ -1,10 +1,8 @@
-const cardRenderLocation = document.getElementById("card-render-location");
-const voteModalImageLocation = document.getElementById("vote-modal-image");
 const onGridClass = "on-grid";
 
 const card = new Card();
 const ui = new Card();
-const rating = new Rating(undefined, undefined, {});
+
 
 function hoverBlurr(element, type){
     
@@ -23,18 +21,6 @@ function hoverBlurr(element, type){
             default: console.alert("try refreshing the page - something went wrong");
                 return;
         } 
-}
-
-function getImageOfCard(card){
-    return card.firstElementChild.currentSrc;
-}
-
-function cardClick(element){
-
-    voteModalImageLocation.attributes.src.value = getImageOfCard(element);
-
-    $("#vote-modal").modal('toggle');
-
 }
 
 function getName(element){
@@ -58,33 +44,4 @@ function cardSelect(element, item){
         card[item] = getName(element);
     }
 
-}
-
-function generateCards(data){
-
-    
-
-    /*
-        <div class="col-sm-12 col-md-6 col-lg-3 card" onclick="cardClick(this)" 
-        onmouseleave="hoverBlurr(this, 'leave')" onmouseover="hoverBlurr(this, 'enter')">
-            <img src="https://gamepedia.cursecdn.com/l5r_gamepedia_en/6/6c/Shrewd_Yasuki.png?version=52fa901f0d3c41c924603c6d4161dfce">
-            <h3 class="card-rating">Rating: 4</h3>
-        </div>
-
-    */
-}
-
-function vsClanModal(element, clan){
-
-    element.classList.toggle("btn-" + clan + "-selected");
-
-    for(let i in rating.clans){
-        console.log(i);
-    }
-
-    debugger;
-
-    addElement(rating.clans, clan);
-
-    
 }
