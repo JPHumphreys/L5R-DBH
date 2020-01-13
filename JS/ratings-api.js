3791f0c100fbb08cd4641fb36e7311c8c5944198
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-01-13 14:13:18 +0000

    created a ratings api to abide by the SOLID principles

---
+++ JS/ratings-api.js
@@ -0,0 +1,55 @@
+const addRatingValue = document.getElementById("add-rating-text");
+const voteModalImageLocation = document.getElementById("vote-modal-image");
+
+const rating = new Rating(undefined, undefined, {});
+
+const ratingJSON = {
+    "ratingcrab":0.0,
+    "ratingcrane":0.0,
+    "ratingdragon":0.0,
+    "ratinglion":0.0,
+    "ratingphoenix":0.0,
+    "ratingscorpion":0.0,
+    "ratingunicorn":0.0
+};
+
+function handleRatingVote(){
+
+    if(addRatingValue.value >= 1.0 && addRatingValue.value <= 10.0){
+
+
+        
+        //* push to API **/
+
+    }else{
+        alert("please type a rating that is between 1.0 and 10.0");
+    }
+}
+
+function makeRatingPushRequest(id, rating, clans){
+
+
+    /*
+        ratingAddReq.open("PUT", addRatingURL + "crab" + "/" + id);
+                ratingAddReq.setRequestHeader("Content-Type", "application/json");
+            obj = {
+                    "ratingcrab":ratingVariableNames[i],
+                };
+                saveRating(id, currentRating, "crab");
+                ratingAddReq.send(JSON.stringify(obj));
+
+    */
+}
+
+getCardRequest.onload = () => {
+    //debugger;
+    
+    if(getCardRequest.status === 200){
+       
+        //* it was a success
+    }
+    else{
+        
+        //* it was not a success
+    }
+}
